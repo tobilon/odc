97554bb6b6654bc4806fc82363cf5c1c27c2f43d
tobilon <tobilon@sina.com>
2014-10-15 16:45:13 +0800

    Update geometry.erl

--- erl/geometry.erl
+++ erl/geometry.erl
@@ -3,4 +3,4 @@
 
 area({rectangle, Width, Height}) -> Width * Height;
 area({square, Side}) -> Side * Side;
-area({circle, Radius}) -> 3.14159 * Radius * Radius.
+area({circle, Radius}) -> 3.1415 * Radius * Radius.
